ceae9b56b08cbdfbe26c66362574fb7f5783e29f
joamona <joamona@cgf.upv.es>
2025-01-21 19:06:03 +0100

    Added cors headers app

--- deswebapi/deswebapi/settings.py
+++ deswebapi/deswebapi/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
+if DEBUG:
+    CORS_ALLOW_ALL_ORIGINS=True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
